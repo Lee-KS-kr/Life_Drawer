bad18eeab438e545f10ee05e6c6c6b76cb362e46
Mizu <KyungseoLee0815@gmail.com>
2022-12-20 11:27:15 +0900

    Line drawing modified

--- Life Drawer/Assets/Mizu/Scripts/Pen.cs
+++ Life Drawer/Assets/Mizu/Scripts/Pen.cs
@@ -7,17 +7,17 @@ namespace Mizu
     public class Pen : MonoBehaviour
     {
         [SerializeField] private LineRenderer _lineRenderer;
-        //[SerializeField] private EdgeCollider2D _edgeCollider;
-        [SerializeField] private MeshCollider _meshCollider;
+        [SerializeField] private EdgeCollider2D _edgeCollider;
+        [Range(0.1f, 1.5f)][SerializeField] private float penLineWidth = 0.5f;
         private int positionCount = 0;
         private int positionIndex = -1;
 
-        private Vector2 _startInput;
-        private Vector2 _endInput;
+        private Vector3 _startInput;
+        private Vector3 _endInput;
+        private Vector3 _camPosZ;
         private List<Vector2> points;
-        private Mesh _mesh;
 
-        private Camera _cam;
+        [SerializeField] private Camera _cam;
 
         private bool canDraw = true;
         public bool IsCovered { get; private set; } = false;
@@ -28,16 +28,16 @@ namespace Mizu
             positionIndex = -1;
 
             _lineRenderer.positionCount = positionCount;
-            _lineRenderer.startWidth = 0.1f;
-            _lineRenderer.endWidth = 0.1f;
+            _lineRenderer.startWidth = 1f;
+            _lineRenderer.endWidth = 1f;
 
-            _mesh = new Mesh();
             points = new List<Vector2>();
         }
 
         private void Start()
         {
             _cam = Camera.main;
+            _camPosZ = _cam.transform.position;
         }
 
         private void Update()
@@ -50,20 +50,20 @@ namespace Mizu
         {
             if (Input.GetMouseButtonDown(0))
             {
-                _startInput = _cam.ScreenToWorldPoint(Input.mousePosition);
+                _startInput = _cam.ScreenToWorldPoint(Input.mousePosition) - _camPosZ;
                 AddPoints(_startInput);
             }
             if (Input.GetMouseButton(0))
             {
-                var pos = _cam.ScreenToWorldPoint(Input.mousePosition);
-                var dist = Vector2.SqrMagnitude(points[positionIndex] - (Vector2)pos);
+                var pos = _cam.ScreenToWorldPoint(Input.mousePosition) - _camPosZ;
+                var dist = Vector3.SqrMagnitude(points[positionIndex] - (Vector2)pos);
                 var want = Mathf.Pow(0.2f, 2);
                 if (dist > want)
                     AddPoints(pos);
             }
             if (Input.GetMouseButtonUp(0))
             {
-                _endInput = _cam.ScreenToWorldPoint(Input.mousePosition);
+                _endInput = _cam.ScreenToWorldPoint(Input.mousePosition) - _camPosZ;
                 AddPoints(_endInput);
 
                 EndDrawing();
@@ -74,6 +74,7 @@ namespace Mizu
         {
             positionCount++;
             positionIndex++;
+
             _lineRenderer.positionCount = positionCount;
             _lineRenderer.SetPosition(positionIndex, newPoint);
             points.Add(newPoint);
@@ -83,8 +84,7 @@ namespace Mizu
         {
             canDraw = false;
 
-            _lineRenderer.BakeMesh(_mesh, true);
-            _meshCollider.sharedMesh = _mesh;
+            _edgeCollider.SetPoints(points);
         }
     }
 }
